a05828e9e572860c3f11d1c531528334009f5a47
sky-gx <92632463+sky-gx@users.noreply.github.com>
2021-11-29 14:45:51 +0800

    实现查询新闻评论的Controller

--- src/main/java/com/nosql/work/Controller/IndexController.java
+++ src/main/java/com/nosql/work/Controller/IndexController.java
@@ -1,15 +1,40 @@
 package com.nosql.work.Controller;
 
+import com.nosql.work.entity.mongo.Comments;
+import com.nosql.work.service.MongoCommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.aggregation.ArrayOperators;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Calendar;
+import java.util.List;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping("/news")
-    public String news(){
-        return "news";
+    @Autowired
+    private MongoCommentService mongoCommentService;
+    /**
+     * 根据id跳转相关新闻页面并获取相关评论
+     * @return
+     */
+    @RequestMapping("/news/{id}")
+    public ModelAndView news(@PathVariable("id") Integer id){
+        ModelAndView modelAndView = new ModelAndView();
+        List<Comments> lists = null;
+        lists = mongoCommentService.findAllComments(id);
+        for (Comments comment:
+             lists) {
+            Calendar calendar = Calendar.getInstance();
+
+        }
+        modelAndView.addObject("comments",lists);
+        modelAndView.setViewName("news");
+        return modelAndView;
     }
 
 
