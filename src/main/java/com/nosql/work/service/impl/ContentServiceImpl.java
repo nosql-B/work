1bacf3a515b8bf6482a6b852eaee4380a4d8ef6d
CCZU_mnww <62826838+catalystcn@users.noreply.github.com>
2021-11-30 19:16:07 +0800

    Add files via upload

---
+++ src/main/java/com/nosql/work/service/impl/ContentServiceImpl.java
@@ -0,0 +1,23 @@
+package com.nosql.work.service.impl;
+
+import com.nosql.work.entity.Content;
+import com.nosql.work.mapper.ContentMapper;
+import com.nosql.work.service.ContentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class ContentServiceImpl implements ContentService {
+
+    @Resource
+    private ContentMapper contentMapper;
+
+
+    @Override
+    public List<Content> findAll(int id) {
+        return contentMapper.findAll(id);
+    }
+}
