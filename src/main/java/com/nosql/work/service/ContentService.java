5631a7ee824288a9d82f86411fdddff5d2bc9e0a
CCZU_mnww <62826838+catalystcn@users.noreply.github.com>
2021-11-30 19:16:46 +0800

    Add files via upload

---
+++ src/main/java/com/nosql/work/service/ContentService.java
@@ -0,0 +1,15 @@
+package com.nosql.work.service;
+
+import com.nosql.work.entity.Content;
+
+import java.util.List;
+
+public interface ContentService {
+
+    /**
+     * 根据id查询相关新闻内容
+     * @param id
+     * @return
+     */
+    public List<Content> findAll(int id);
+}
