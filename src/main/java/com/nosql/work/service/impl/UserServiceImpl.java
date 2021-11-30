58f764cd7488d781482db12910276b6a02ba60da
yeqiC <52601586+yeqiC@users.noreply.github.com>
2021-11-30 22:08:37 +0800

    Add files via upload

---
+++ src/main/java/com/nosql/work/service/impl/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package com.nosql.work.service.impl;
+
+import com.nosql.work.entity.User;
+import com.nosql.work.mapper.UserMapper;
+import com.nosql.work.service.UserService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Resource
+    private UserMapper userMapper;
+
+    /**
+     * 根据用户名查询相关用户
+     * @param name
+     * @return
+     */
+    @Override
+    public List<User> selectUserByName(String name) {
+        List<User> users = null;
+        users = userMapper.selectUserByName(name);
+        return users;
+    }
+
+    /**
+     * 实现注册功能
+     * @param user
+     */
+    @Override
+    public void insert(User user) {
+        userMapper.insert(user);
+    }
+
+
+}
